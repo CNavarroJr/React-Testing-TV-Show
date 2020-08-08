7400eca821aca26a0521427ac1980fcf445fe2ce
Junior Navarro <cjuniordse@Juniors-Mac-mini.local>
2020-08-07 21:02:45 -0500

    MVP finished

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import axios from "axios";
 
-export const fetchShow = () = > {
-  return
-}
+export const fetchShow = () => {
+  return axios
+    .get(
+      "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    )
+    .then((res) => {
+      // console.log(res)
+      return res;
+    })
+  .catch ((err) => {
+    console.log(err.message)
+    return err
+  })
+};
